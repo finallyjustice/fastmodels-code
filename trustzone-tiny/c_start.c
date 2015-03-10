01526f00ce1a4081cd86f11263a03985b2770344
finallyjustice <dongli.zhang0129@gmail.com>
2015-03-10 19:29:00 -0400

    add tiny monitor

---
+++ trustzone-tiny/c_start.c
@@ -0,0 +1,110 @@
+#include "semihosting.h"
+
+#define __REG(x)    (*((volatile unsigned int *)(x)))
+#define UART0           0x1c090000
+
+void uart_send(const char c)
+{
+	__REG(UART0) = c;
+	if(c == '\n')
+		__REG(UART0) = '\r';
+}
+
+void printint(int xx, int base, int sign)
+{
+	static char digits[] = "0123456789abcdef";
+	char buf[16];
+	int i;
+	unsigned int x;
+
+	if(sign && (sign = xx < 0))
+		x = -xx;
+	else
+		x = xx;
+
+	i = 0;
+	do{
+		buf[i++] = digits[x % base];
+	}while((x /= base) != 0);
+
+	if(sign)
+		buf[i++] = '-';
+
+	while(--i >= 0)
+		uart_send(buf[i]);
+}
+
+void cprintf(char *fmt, ...)
+{
+	int i, c;
+	unsigned int *argp;
+	char *s;
+
+	argp = (unsigned int*)(void*)(&fmt + 1);
+	for(i = 0; (c = fmt[i] & 0xff) != 0; i++)
+	{
+		if(c != '%')
+		{
+			uart_send(c);
+			continue;
+		}
+		c = fmt[++i] & 0xff;
+		if(c == 0)
+			break;
+		switch(c)
+		{
+		case 'd':
+			printint(*argp++, 10, 1);
+			break;
+		case 'x':
+		case 'p':
+			printint(*argp++, 16, 0);
+			break;
+		case 's':
+			if((s = (char*)*argp++) == 0)
+				s = "(null)";
+			for(; *s; s++)
+				uart_send(*s);
+			break;
+		case '%':
+			uart_send('%');
+			break;
+		default:
+			// Print unknown % sequence to draw attention.
+			uart_send('%');
+			uart_send(c);
+			break;
+		}
+	}
+}
+
+void Normal_World()
+{
+	while(1)
+	{
+		//semi_write0("[Fast Model] This is normal world\n");
+		cprintf("[Fast Model] This is normal world\n");
+		asm volatile(
+				".arch_extension sec\n\t"
+				"smc #0\n\t") ;
+	}
+}
+
+void c_start(void)
+{
+	monitorInit(Normal_World);
+	//semi_write0("[Fast Model] Install Monitor Successfully\n");
+	cprintf("[Fast Model] Install Monitor Successfully\n");
+
+	int i;
+	for(i=0; i<10; i++)
+	{
+		//semi_write0("[Fast Model] This is secure world\n");
+		cprintf("[Fast Model] This is secure world\n");
+		asm volatile(
+				".arch_extension sec\n\t"
+				"smc #0\n\t");
+	};
+
+	while(1);
+}
